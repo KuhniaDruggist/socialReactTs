1ab3d3fdde4a89764d59f13351c9792e45b3ca8f
HP <druggist5059@gmail.com>
2021-08-04 13:09:16 +0300

    Adds stub pages

---
+++ src/components/Friends/Friends.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Friends() {
+    return (
+        <div>
+            Information about your friends will be here
+        </div>
+    );
+}
+
+export default Friends
---
+++ src/components/News/News.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function News() {
+    return (
+        <div>
+            Here will be news
+        </div>
+    );
+}
+
+export default News
