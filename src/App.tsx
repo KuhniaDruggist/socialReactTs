4219ec68d85f63b8ae604f4bbacd6ed388054548
HP <druggist5059@gmail.com>
2021-08-07 15:21:10 +0300

    Refactor BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import News from './components/News/News';
 import Friends from './components/Friends/Friends';
 import {RootStateType} from './redux/state';
@@ -15,18 +15,16 @@ type PropsType = {
 
 function App(props: PropsType) {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar state={props.state.navbar}/>
-                <div className="app-wrapper-content">
-                    <Route path='/profile' render={ () => <Profile state={props.state.profilePage}/> } />
-                    <Route path='/news' render={ () => <News/> } />
-                    <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage}/> } />
-                    <Route path='/friends' render={ () => <Friends/> } />
-                </div>
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar state={props.state.navbar}/>
+            <div className="app-wrapper-content">
+                <Route path="/profile" render={() => <Profile state={props.state.profilePage}/>}/>
+                <Route path="/news" render={() => <News/>}/>
+                <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                <Route path="/friends" render={() => <Friends/>}/>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
