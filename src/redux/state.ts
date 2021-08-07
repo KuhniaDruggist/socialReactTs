b48e15ce15cae69c4c58956a0f0ffc6fae27c543
HP <druggist5059@gmail.com>
2021-08-07 13:31:41 +0300

    Add data in state

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -18,10 +18,12 @@ export type PostType = {
 export type DialogType = {
     id: number
     name: string
+    photo: string
 }
 export type MessageType = {
     id?: number
     message: string
+    position: string
 }
 
 let state: RootStateType = {
@@ -43,18 +45,18 @@ let state: RootStateType = {
     },
     dialogsPage: {
         dialogs: [
-            {id: 1, name: 'Виталий'},
-            {id: 2, name: 'Себастьян'},
-            {id: 3, name: 'Анжела Перл'},
-            {id: 4, name: 'Кристофер'},
-            {id: 5, name: 'Мирослава'}
+            {id: 1, name: 'Виталий', photo: 'https://i.pinimg.com/736x/52/cc/bc/52ccbc8cc85e151b590476e1a815a96d.jpg'},
+            {id: 2, name: 'Себастьян', photo: 'https://image.freepik.com/free-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg'},
+            {id: 3, name: 'Анжела Перл', photo: 'https://sunmag.me/wp-content/uploads/2019/11/sunmag-004-small-avatar.png'},
+            {id: 4, name: 'Кристофер', photo: 'https://i.pinimg.com/originals/e4/55/d2/e455d2e6e205c8eeeda1356a00cc6bfb.jpg'},
+            {id: 5, name: 'Мирослава', photo: 'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg'}
         ],
         messages: [
-            {id: 1, message: 'Hi!'},
-            {id: 2, message: 'Hi. How are you?'},
-            {id: 3, message: 'I am fine. Thank you.'},
-            {id: 4, message: 'Where is my money?'},
-            {id: 5, message: 'Bye)))'}
+            {id: 1, message: 'Hi!', position: 'left'},
+            {id: 2, message: 'Hi. How are you?', position: 'right'},
+            {id: 3, message: 'I am fine. Thank you.', position: 'left'},
+            {id: 4, message: 'Where is my money?', position: 'right'},
+            {id: 5, message: 'Bye)))', position: 'left'}
         ]
     }
 };
