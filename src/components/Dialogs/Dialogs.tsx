d7a1f9f845d2d8a0c6909f2c367a84dae4246e55
HP <druggist5059@gmail.com>
2021-08-16 22:15:42 +0300

    Add property key. Add block with field for adding message

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -9,8 +9,8 @@ type PropsType = {
 }
 
 function Dialogs(props: PropsType) {
-    let dialogElements = props.state.dialogs.map(d => <DialogItem id={d.id} name={d.name} photo={d.photo}/>)
-    let messageElements = props.state.messages.map(m => <Message  message={m.message} position={m.position}/>)
+    let dialogElements = props.state.dialogs.map(d => <DialogItem key={d.id} id={d.id} name={d.name} photo={d.photo}/>)
+    let messageElements = props.state.messages.map(m => <Message  key={m.id} message={m.message} position={m.position}/>)
 
     return (
         <div className={style.dialogs}>
@@ -18,9 +18,15 @@ function Dialogs(props: PropsType) {
             <ul className={`${style.list} ${style.dialogsList}`}>
                 { dialogElements }
             </ul>
-            <ul className={`${style.list} ${style.messagesList}`}>
-                { messageElements }
-            </ul>
+            <div className={style.messagesWrapper}>
+                <ul className={`${style.list} ${style.messagesList}`}>
+                    { messageElements }
+                </ul>
+                <div className={style.messageForm}>
+                    <textarea className={style.textMessage} name="newMessage" placeholder="Type a message" />
+                    <button  className={style.messageButton} type="button">Send</button>
+                </div>
+            </div>
         </div>
     );
 }
