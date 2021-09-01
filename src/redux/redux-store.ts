ef6ab502c5168588ed246cbe873cad262ae5f8dc
KuhniaDruggist <druggist5059@gmail.com>
2021-09-01 20:12:08 +0300

    Renames root reducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -3,12 +3,12 @@ import {profileReducer} from './profileReducer';
 import {dialogsReducer} from './dialogsReducer';
 import {navbarReducer} from './navbarReducer';
 
-let reducers = combineReducers({
+const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     navbar: navbarReducer
 })
 
-export type RootStateType = ReturnType<typeof reducers>
+export type RootStateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(reducers);
+export const store = createStore(rootReducer);
