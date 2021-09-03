f78a77b51caafc90fdd193aee2f3c60882b1f7f8
KuhniaDruggist <druggist5059@gmail.com>
2021-09-03 23:24:49 +0300

    Simplifies component App. Gets rid of props.

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,20 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Friends from './components/Friends/Friends';
-import {RootStateType} from './redux/redux-store';
-import {DialogsActionTypes} from './redux/dialogsReducer';
-import {ProfileActionTypes} from './redux/profileReducer';
+import {DialogsContainer} from './components/Dialogs/DialogsContainer';
 
-type AppPropsType = {
-    state: RootStateType
-    dispatch: (action: DialogsActionTypes | ProfileActionTypes) => void
-}
-
-function App(props: AppPropsType) {
+function App() {
     return (
         <div className="app-wrapper">
             <Header/>
-            <Navbar state={props.state.navbar}/>
+            <Navbar/>
             <div className="app-wrapper-content">
-                <Route path="/profile" render={() => <Profile state={props.state.profilePage}
-                                                              dispatch={props.dispatch}/>}/>
+                <Route path="/profile" render={() => <Profile/>}/>
                 <Route path="/news" render={() => <News/>}/>
                 <Route path="/dialogs"
-                       render={() => <Dialogs state={props.state.dialogsPage} dispatch={props.dispatch}/>}/>
+                       render={() => <DialogsContainer/>}/>
                 <Route path="/friends" render={() => <Friends/>}/>
             </div>
         </div>
