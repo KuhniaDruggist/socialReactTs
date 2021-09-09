b215b734212acc06be57c8b728609c4a9b0bbc95
KuhniaDruggist <druggist5059@gmail.com>
2021-09-09 21:35:07 +0300

    connects a new reducer Users

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -2,10 +2,12 @@ import {combineReducers, createStore} from 'redux';
 import {profileReducer} from './profileReducer';
 import {dialogsReducer} from './dialogsReducer';
 import {navbarReducer} from './navbarReducer';
+import {usersReducer} from './usersReducer';
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
+    usersPage: usersReducer,
     navbar: navbarReducer
 })
 
