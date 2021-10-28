af7be658e1cdb5c521deda58df287f1ff37520d4
KuhniaDruggist <druggist5059@gmail.com>
2021-10-28 23:00:43 +0300

    Replaces a functional component with a class. Makes a request to the server to get users.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,62 +1,48 @@
 import React from 'react';
 import style from './Users.module.css'
+import userPhotoDefault from '../../assets/img/user.png';
+import axios from 'axios';
 import {UsersPropsType} from './UsersContainer';
 
-function Users(props: UsersPropsType) {
-    const users = [
-        {
-            name: 'Ivan',
-            id: 1,
-            photos: {
-                small: 'https://pbs.twimg.com/media/EgX6nMxUYAAirGg.jpg',
-                large: null
-            },
-            status: 'I love free Belarus',
-            followed: true
-        },
-        {
-            name: 'Angelika',
-            id: 2,
-            photos: {
-                small: 'http://www.dejurka.ru/wp-content/uploads/2018/10/cute-cats-draw4.jpg',
-                large: null
-            },
-            status: 'I love my cat Semyon',
-            followed: false
-        }
-    ]
-
-    if (props.users.length === 0) {
-        props.setUsers(users);
+class Users extends React.Component<UsersPropsType>{
+    
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
     }
 
-    return (
-        <ul className={style.users}>
-            {
-                props.users.map(u => {
-                    const buttonClass = u.followed ? style.follow : style.unfollow;
-                    const onClickToggle = () => props.toggleFollowing(u.id);
+    render() {
+        return (
+            <ul className={style.users}>
+                {
+                    this.props.users.map(u => {
+                        const buttonClass = u.followed ? style.follow : style.unfollow;
+                        const onClickToggle = () => this.props.toggleFollowing(u.id);
 
-                    return (
-                        <li className={style.user} key={u.id}>
-                            <div className={style.header}>
-                                <div className={style.avatarWrapper}>
-                                    <img className={style.avatar} alt="Аватар пользователя"
-                                         src={u.photos.small != null ? u.photos.small : ''}/>
+                        return (
+                            <li className={style.user} key={u.id}>
+                                <div className={style.header}>
+                                    <div className={style.avatarWrapper}>
+                                        <img className={style.avatar} alt="Аватар пользователя"
+                                             src={u.photos.small != null ? u.photos.small : userPhotoDefault}/>
+                                    </div>
+                                    <button className={`${style.button} ${buttonClass}`}
+                                            onClick={onClickToggle}>{u.followed ? 'unfollow' : 'follow'}</button>
                                 </div>
-                                <button className={`${style.button} ${buttonClass}`}
-                                        onClick={onClickToggle}>{u.followed ? 'unfollow' : 'follow'}</button>
-                            </div>
-                            <div className={style.body}>
-                                <h3 className={style.name}>{u.name}</h3>
-                                <p className={style.status}>{u.status}</p>
-                            </div>
-                        </li>
-                    );
-                })
-            }
-        </ul>
-    );
+                                <div className={style.body}>
+                                    <h3 className={style.name}>{u.name}</h3>
+                                    <p className={style.status}>{u.status}</p>
+                                </div>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
+        );
+    }
+
 }
 
 export default Users
